63e31415576a9a7ac3f1021ad78f9c980f225654
EC2 Default User <ec2-user@ip-172-31-22-56.ap-southeast-1.compute.internal>
2021-11-10 14:06:29 +0000

    test new view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_065027) do
+ActiveRecord::Schema.define(version: 2021_11_02_104858) do
+
+  create_table "fish", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.boolean "like_area_id"
